764f11c4741c40da1000ff1da626f3edf67a256d
Henrique <henrique.c.ladeira@gmail.com>
2021-06-30 23:47:01 -0300

    ✨ feat: add material ui theme

--- src/index.js
+++ src/index.js
@@ -2,11 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './fonts.css';
 import './index.css';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core';
 import App from './App';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#e6a727',
+    },
+    text: 'white',
+  },
+  typography: {
+    fontFamily: ['Lato'],
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
