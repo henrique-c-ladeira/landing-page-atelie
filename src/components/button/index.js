623ca4ffde618f77e0c82f6a5987f79594b66337
Henrique <henrique.c.ladeira@gmail.com>
2021-07-01 00:31:34 -0300

    🩹 fix: button children

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button as SButton } from './styles';
 
-export const Button = (props) => (
-  <SButton type="button" {...props} />
+export const Button = ({ children, props }) => (
+  <SButton type="button" {...props}>{children}</SButton>
 );
