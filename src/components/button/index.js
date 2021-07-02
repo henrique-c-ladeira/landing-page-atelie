57f48331df4525772b7372f3c1397df1a57d1d7b
Henrique <henrique.c.ladeira@gmail.com>
2021-07-01 21:15:46 -0300

    🐛 fix: wrong props to button

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button as SButton } from './styles';
 
-export const Button = ({ children, props }) => (
+export const Button = ({ children, ...props }) => (
   <SButton type="button" {...props}>{children}</SButton>
 );
