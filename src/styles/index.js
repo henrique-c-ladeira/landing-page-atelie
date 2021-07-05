091a8d4feb260bd19c03ccffd928491e93aa2c17
Henrique <henrique.c.ladeira@gmail.com>
2021-07-04 21:40:02 -0300

    💄 feat: add standard button style

---
+++ src/styles/index.js
@@ -0,0 +1,18 @@
+import { css } from 'styled-components';
+
+export const ButtonStandard = css`
+  color: white;
+  background-color: #e6a727;
+  border-radius: 5px;
+  text-transform: uppercase;
+  padding: 15px 30px;
+  cursor: pointer;
+  z-index: 1;
+
+  font-size: 16px;
+  font-weight: 600;
+
+  &:active {
+    transform: scale(0.95);
+  }
+`;
